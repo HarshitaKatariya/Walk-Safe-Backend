97ab01fe06e8d6169e68f9987b11ed98548abe15
HK <harshitakatariya1508@gmail.com>
2025-08-06 16:53:11 +0530

    add change in firebase.js

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,10 +1,11 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('../serviceAccountKey.json');
 
+// Load service account from environment variable
+const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://walksafe-44d43-default-rtdb.asia-southeast1.firebasedatabase.app/" 
-})
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://walksafe-44d43-default-rtdb.asia-southeast1.firebasedatabase.app/"
+});
 
 module.exports = admin;
